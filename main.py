9dcac3f28828e46365ffd634a592d85daaa8d0f9
Cooldude1259 <lucas.chrisopoulos@gmail.com>
2025-12-18 17:20:19 +1030

    try something different

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import os
+from flask import Flask
 
 api_key = os.environ.get("API_KEY")
+
+
 
 if api_key is None:
     raise ValueError("API_KEY environment variable is not set.")
 else:
     print("API_KEY is set.")
     print(f"API_KEY: {api_key}")
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, World!"
